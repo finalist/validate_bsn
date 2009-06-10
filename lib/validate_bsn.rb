930b7ea45849d1745d5b0b6c2b80b431a4ee24cb
Iain Hecker <github@iain.nl>
2009-06-10 20:55:51 +0200

    performance increase

--- lib/validate_bsn.rb
+++ lib/validate_bsn.rb
@@ -1,9 +1,10 @@
 module ValidateBSN
 
   def self.valid?(value)
-    return false if value.to_i <= 99_999_999 or value.to_i >= 999_999_999
-    j, factors = -1, [9,8,7,6,5,4,3,2,-1]
-    value.to_s.split(//).inject(0) do |sum, i|
+    v = value.to_i
+    return false if v <= 99_999_999 or v >= 999_999_999
+    j, sum, factors = -1, 0, [9,8,7,6,5,4,3,2,-1]
+    value.to_s.each do |i|
       sum += i.to_i * factors[j += 1]
     end % 11 == 0
   end
