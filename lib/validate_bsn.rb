c8b7975a622e80b34725178c8f5cb334afd5c950
Iain Hecker <github@iain.nl>
2009-06-10 21:30:25 +0200

    Added Rails initializer

--- lib/validate_bsn.rb
+++ lib/validate_bsn.rb
@@ -1,19 +1,24 @@
 module ValidateBSN
 
   def self.valid?(value)
-    v = value.to_i
-    return false if v <= 99_999_999 or v >= 999_999_999
-    j, sum, factors = -1, 0, [9,8,7,6,5,4,3,2,-1]
-    value.to_s.each do |i|
-      sum += i.to_i * factors[j += 1]
+
+    int_value = value.to_i
+    if int_value <= 99_999_999 or int_value >= 999_999_999
+      return false
+    end
+
+    factor_index, sum, factors = -1, 0, [9,8,7,6,5,4,3,2,-1]
+    value.to_s.each do |char|
+      sum += char.to_i * factors[factor_index += 1]
     end % 11 == 0
+
   end
 
-  module ActiveRecordClassMethods
+  module ActiveRecord
 
     def validates_bsn_format(*args)
-      validates_each *args do |record, attr, value|
-        record.errors.add(attr, :invalid_bsn) unless ValidateBSN.valid?(value)
+      validates_each(*args) do |record, attr, value|
+        record.errors.add(attr) unless ValidateBSN.valid?(value)
       end
     end
 
