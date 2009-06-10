e601690506629250a190bb7104e5a8b3733946ba
Iain Hecker <github@iain.nl>
2009-06-10 22:34:28 +0200

    added rspec macros and matchers

--- lib/validate_bsn.rb
+++ lib/validate_bsn.rb
@@ -19,11 +19,58 @@ module ValidateBSN
   module ActiveRecord
 
     def validates_bsn_format(*args)
+      @__validates_bsn_format = args
       validates_each(*args) do |record, attr, value|
         record.errors.add(attr) unless ValidateBSN.valid?(value)
       end
     end
 
+  end
+
+  module Matchers
+    class ValidateBSNMatcher
+      def initialize(model, *args)
+        @model = model
+        @expected = args
+      end
+
+      def matches?(*args)
+        @actual = @model.class.instance_variable_get(:@__validates_bsn_format)
+        @actual == @expected
+      end
+
+      def description
+        "should have a validated BSN format"
+      end
+
+      def failure_message_for_should
+        "expected #{@model.class.human_name} to have these BSN validation options: #{@expected.inspect}, but got #{@actual.inspect}"
+      end
+
+      def failure_message_for_should_not
+        "expected #{@model.class.human_name} not to have these BSN validation options: #{@expected.inspect}, but got #{@actual.inspect}"
+      end
+    end
+
+    def validate_bsn_format(*args)
+      ValidateBSNMatcher.new(subject, *args)
+    end
+  end
+
+  module Macros
+    def self.included(base)
+      base.send(:include, ValidateBSN::Matchers)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def should_validate_bsn_format(*args)
+        it { should validate_bsn_format(*args) }
+      end
+      def should_not_validate_bsn_format(*args)
+        it { should_not validate_bsn_format(*args) }
+      end
+    end
   end
 
 end
